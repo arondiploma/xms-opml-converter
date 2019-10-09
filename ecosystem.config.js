4e623c4bd3cf4afa290c3ec2e0ad5c5598d4e28c
zerocoolElite <arondiploma@gmail.com>
2019-10-09 14:52:31 +0800

    Create ecosystem.config.js

---
+++ ecosystem.config.js
@@ -0,0 +1,14 @@
+//PM2 config
+module.exports = {
+    apps: [
+        {
+            name: "myapp",
+            script: "./dist/bin/www.js",
+            watch: true,
+            env: {
+                "NODE_ENV": "production",
+                "PORT": 80,
+            }
+        }
+    ]
+}
